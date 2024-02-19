8f1b27edf44e6c384eee26a61b51b8c12c8ea1d9
David Ibarra <ibarrarojashector@gmail.com>
2024-02-19 17:43:15 -0400

    update

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -2,43 +2,24 @@ import { Producto } from "../interfaces/productos";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Styles from "./ProductsList.module.css";
-import { NavLink, useSearchParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function ProductsList() {
   const [datos, setDatos] = useState<Producto[]>([]);
-  const [searchParams] = useSearchParams();
 
-  async function getData(category?: string, brand?: string) {
+  async function getData() {
     try {
-      const url = category
-        ? `https://dummyjson.com/products/category/${category}`
-        : "https://dummyjson.com/products";
-      const res = await axios.get(url);
+      const res = await axios.get("https://dummyjson.com/products");
       const data = res.data;
       const productos = data.products;
-      if (brand) {
-        const filteredData = productos.filter(
-          (product: Producto) => product.brand === brand
-        );
-        setDatos(filteredData);
-      } else {
-        setDatos(productos);
-      }
+      setDatos(productos);
     } catch (error) {
       console.error("Error fetching data", error);
     }
   }
-  useEffect(() => {
-    const brand = searchParams.get("brand");
-    const category = searchParams.get("category");
-    console.log(category);
-    console.log(brand);
-    getData(String(category), String(brand));
-  }, [searchParams]);
 
   useEffect(() => {
     getData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function descuento(price: number, discountPercentage: number) {
@@ -54,19 +35,15 @@ export default function ProductsList() {
             <NavLink to={`/products/${producto.id}`}>
               <img src={producto.thumbnail} alt="product" className="imagen" />
             </NavLink>
-
-            <NavLink to={`/products?category=${producto.category}`}>
+            <NavLink to={`/products/category/${producto.category}`}>
               <p className="category">{producto.category}</p>
             </NavLink>
-
             <NavLink to={`/products/${producto.id}`}>
               <h2 className="title">{producto.title}</h2>
             </NavLink>
-
-            <NavLink to={`/products?brand=${producto.brand}`}>
+            <NavLink to={`/products/brand/${producto.brand}`}>
               <h4 className="sub">{producto.brand}</h4>
             </NavLink>
-
             <div className="body">
               <del style={{ color: "red" }}>Precio : {producto.price}</del>
               <h5>
