821f5872e4732212c408836259548ef38c42641f
David Ibarra <ibarrarojashector@gmail.com>
2024-02-21 15:15:55 -0400

    backup

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,3 +1,72 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Producto } from "../interfaces/productos";
+import Styles from "./styles/ProductPage.module.css";
+
 export default function ProductsPage() {
-  return <div>123</div>;
+  const { id, brand, category } = useParams<{
+    id: string;
+    brand: string;
+    category: string;
+  }>();
+  const [datos, setDatos] = useState<Producto | null>(null);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const url = category
+          ? `https://dummyjson.com/products/category/${category}`
+          : `https://dummyjson.com/products/${id}`;
+        const res = await axios.get(url);
+        const data = res.data;
+        if (brand) {
+          const formatedData = data.filter(
+            (product: Producto) => product.brand === brand
+          );
+          setDatos(formatedData);
+        } else {
+          setDatos(data);
+        }
+
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function descuento(price: number, discountPercentage: number) {
+    const descuento = price * (discountPercentage / 100);
+    return price - descuento;
+  }
+
+  return (
+    <div className={Styles.body}>
+      <div className={Styles.body2}>
+        <div className={Styles.id}>
+          <p>Producto Numero : #{id}</p>
+        </div>
+
+        <div style={{ color: "white" }}>
+          <img src={datos?.thumbnail} alt="Iphone 9" className={Styles.image} />
+          <p className={Styles.title}>{datos?.title}</p>
+          <p>Descripcion : {datos?.description}</p>
+          <p>Marca : {datos?.brand}</p>
+          <p>Categoria : {datos?.category}</p>
+          <p>Rating : {datos?.rating}</p>
+          <del>Precio : {datos?.price}</del>
+          <p>
+            Precio con descuento :{" "}
+            {Math.floor(
+              descuento(datos?.price ?? 0, datos?.discountPercentage ?? 0)
+            ).toString()}
+          </p>
+          <button className={Styles.button}>Añadir</button>
+        </div>
+      </div>
+    </div>
+  );
 }
